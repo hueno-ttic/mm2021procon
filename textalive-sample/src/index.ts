4e7fef75479307d27fa1bef0b9c54ec3de84c646
sakura <sakuramodki@gmail.com>
2021-09-08 00:08:50 +0900

    commit時にlintを行う設定を追加

--- textalive-sample/src/index.ts
+++ textalive-sample/src/index.ts
@@ -1,48 +1,48 @@
-//必要なimport
-import * as Phaser from "phaser";
-import CharacterSelectScene from "./scenes/CharacterSelect";
-import TitleScene from "./scenes/TitleScene";
-import GameMain from "./scenes/GameMain";
-import GameResult from "./scenes/GameResult";
-import MusicSelectScene from "./scenes/MusicSelect";
+// 必要なimport
+import * as Phaser from 'phaser';
+import CharacterSelectScene from './scenes/CharacterSelect';
+import TitleScene from './scenes/TitleScene';
+import GameMain from './scenes/GameMain';
+import GameResult from './scenes/GameResult';
+import MusicSelectScene from './scenes/MusicSelect';
 
-//ゲームの基本設定
+// ゲームの基本設定
 const config: Phaser.Types.Core.GameConfig = {
-    title: "sampleLyric",    //タイトル
-    version: "0.0.1",       //バージョン
-    width: 1280,             //画面幅
-    height: 720,            //画面高さ
-    parent: "game",          //DOM上の親
-    type: Phaser.AUTO,      //canvasかwebGLかを自動選択
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
-    },
-    scene: [
-        TitleScene,
-        GameMain,
-        CharacterSelectScene,
-        MusicSelectScene,
-        GameResult,
-        ]
+  title: 'sampleLyric', // タイトル
+  version: '0.0.1', // バージョン
+  width: 1280, // 画面幅
+  height: 720, // 画面高さ
+  parent: 'game', // DOM上の親
+  type: Phaser.AUTO, // canvasかwebGLかを自動選択
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
+  },
+  scene: [
+    TitleScene,
+    GameMain,
+    CharacterSelectScene,
+    MusicSelectScene,
+    GameResult,
+  ],
 };
 
-//ゲームメインのクラス
+// ゲームメインのクラス
 export class Game extends Phaser.Game {
-    constructor() {
-        super(config);
-    }
+  constructor() {
+    super(config);
+  }
 }
 
 class Main {
-    constructor() { }
-    public initialize() {
-        //windowイベントで、ロードされたらゲーム開始
-        window.addEventListener("load", () => {
-            console.log("start");
-            var gameApp = new Game();
-        });
-    }
+  constructor() {}
+  initialize() {
+    // windowイベントで、ロードされたらゲーム開始
+    window.addEventListener('load', () => {
+      console.log('start');
+      let gameApp = new Game();
+    });
+  }
 }
 
 new Main().initialize();
