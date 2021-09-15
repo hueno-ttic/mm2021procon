d2e83ec4e57a067bfe07ca217a9082593ee224ca
ya2ha4 <>
2021-09-16 03:21:18 +0900

    GameMain に一時停止ボタンを実装

---
+++ textalive-sample/src/object/UIPauseButton.ts
@@ -0,0 +1,65 @@
+import TextaliveApiManager from "../TextaliveApiManager";
+import UIImageButtonObject from "./UIImageButton";
+
+export interface UIPauseButtonObjectCreateParam {
+    scene: Phaser.Scene;
+    pauseImageKey: string;
+    playImageKey: string;
+    imageDepth?: number;
+    posX: number;
+    posY: number;
+    textaliveManager: TextaliveApiManager;
+}
+
+export default class UIPauseButtonObject {
+    private _button: UIImageButtonObject;
+    private _textaliveManager: TextaliveApiManager;
+
+    constructor() {
+        this.init();
+    }
+
+    public init(): void {
+        this._button = null;
+        this._textaliveManager = null;
+    }
+
+    public create(param: UIPauseButtonObjectCreateParam): void {
+        let imageKeyMap = new Map<string, string>([
+            ["pause", param.pauseImageKey],
+            ["play", param.playImageKey]
+        ]);
+
+        this._button = new UIImageButtonObject();
+        this._button.create({
+            scene: param.scene,
+            imageKeyMap: imageKeyMap,
+            posX: param.posX,
+            posY: param.posY,
+            firstStatusName: "pause"
+        });
+        this._button.responseObject.on("pointerdown", () => {
+            this.pointerdown()
+        })
+
+        this._textaliveManager = param.textaliveManager;
+    }
+
+    private pointerdown(): void {
+        if (!this._textaliveManager || this._textaliveManager.player.isLoading) {
+            return;
+        }
+
+        if (this._textaliveManager.player.isPlaying) {
+            this._button.status = "play";
+            this._textaliveManager.player.requestPause();
+        } else {
+            this._button.status = "pause";
+            this._textaliveManager.player.requestPlay();
+        }
+    }
+
+    public setVisible(value: boolean): void {
+        this._button.setVisible(value);
+    }
+}
