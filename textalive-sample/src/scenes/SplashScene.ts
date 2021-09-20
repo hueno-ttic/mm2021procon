ff5477101f38b56fbf7a44c3b9e28f82a5309554
ya2ha4 <>
2021-09-21 03:55:46 +0900

    lint対応

--- textalive-sample/src/scenes/SplashScene.ts
+++ textalive-sample/src/scenes/SplashScene.ts
@@ -15,20 +15,33 @@ export default class SplashScene extends Phaser.Scene {
     create(): void {
         this.cameras.main.fadeIn(1000, 0, 0, 0);
         this.add.image(640, 360, "splash01");
-        this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_IN_COMPLETE, () => {
-            this.cameras.main.fadeOut(1000, 0, 0, 0);
-            this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, () => {
-                this.cameras.main.fadeIn(1000, 0, 0, 0);
-                this.add.image(640, 360, "splash02");
-                this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_IN_COMPLETE, () => {
-                    this.input.on("pointerdown", () => {
-                    this.cameras.main.fadeOut(1000, 0, 0, 0);
-                    this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, () => {
-                            this.scene.start("TitleScene");
-                        });
-                    });
-                });
-            });
-        });
+        this.cameras.main.once(
+            Phaser.Cameras.Scene2D.Events.FADE_IN_COMPLETE,
+            () => {
+                this.cameras.main.fadeOut(1000, 0, 0, 0);
+                this.cameras.main.once(
+                    Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE,
+                    () => {
+                        this.cameras.main.fadeIn(1000, 0, 0, 0);
+                        this.add.image(640, 360, "splash02");
+                        this.cameras.main.once(
+                            Phaser.Cameras.Scene2D.Events.FADE_IN_COMPLETE,
+                            () => {
+                                this.input.on("pointerdown", () => {
+                                    this.cameras.main.fadeOut(1000, 0, 0, 0);
+                                    this.cameras.main.once(
+                                        Phaser.Cameras.Scene2D.Events
+                                            .FADE_OUT_COMPLETE,
+                                        () => {
+                                            this.scene.start("TitleScene");
+                                        }
+                                    );
+                                });
+                            }
+                        );
+                    }
+                );
+            }
+        );
     }
 }
