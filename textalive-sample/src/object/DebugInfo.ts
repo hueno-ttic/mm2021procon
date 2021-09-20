53a5b6d046235db878b4f3e6f28ad76a2cb72e25
ya2ha4 <>
2021-09-21 02:22:29 +0900

    デバッグ表示時 TextAlive App API のパラメータ取得に失敗した場合、try catch で回避

--- textalive-sample/src/object/DebugInfo.ts
+++ textalive-sample/src/object/DebugInfo.ts
@@ -70,50 +70,54 @@ export default class DebugInfo {
         const player = this._textaliveAppApi.player;
         const video = player.video;
 
-        const beat = player.findBeat(position);
-        songInfoList.push(`ビート間隔[ms]: ${beat ? beat.duration : "----"}`);
-        songInfoList.push(`ビート数: ${beat ? beat.length : "----"}`);
-        songInfoList.push(
-            `1小節[ms]: ${beat ? beat.duration * beat.length : "----"}`
-        );
-        songInfoList.push(
-            `BPM: ${beat ? ((60 * 1000) / beat.duration).toFixed(2) : "----"}`
-        );
-
-        songInfoList.push(
-            `サビ: ${player.findChorus(position) ? "True" : "False"}`
-        );
-
-        const chord = player.findChord(position);
-        songInfoList.push(`コード： ${chord ? chord.name : "----"}`);
-
-        const word = video.findWord(position);
-        songInfoList.push(
-            `声量(time): ${this.getFormatted(
-                player.getVocalAmplitude(position)
-            )}`
-        );
-        let wordAmplitude = 0;
-        if (word) {
-            wordAmplitude =
-                (player.getVocalAmplitude(word.startTime) +
-                    player.getVocalAmplitude(word.endTime) +
-                    player.getVocalAmplitude(
-                        (word.startTime + word.endTime) / 2
-                    )) /
-                3;
-        }
-        songInfoList.push(`声量(word): ${this.getFormatted(wordAmplitude)}`);
-
-        // Uncaught TypeError: Cannot read properties of undefined 回避のための処理
-        if (position < video.lastChar.endTime) {
-            const va = player.getValenceArousal(position);
+        try {
+            const beat = player.findBeat(position);
+            songInfoList.push(`ビート間隔[ms]: ${beat ? beat.duration : "----"}`);
+            songInfoList.push(`ビート数: ${beat ? beat.length : "----"}`);
             songInfoList.push(
-                `V/A(time): 感情価 ${this.getFormatted(
-                    va.v,
-                    4
-                )}, 覚醒度 ${this.getFormatted(va.a, 4)}`
+                `1小節[ms]: ${beat ? beat.duration * beat.length : "----"}`
             );
+            songInfoList.push(
+                `BPM: ${beat ? ((60 * 1000) / beat.duration).toFixed(2) : "----"}`
+            );
+    
+            songInfoList.push(
+                `サビ: ${player.findChorus(position) ? "True" : "False"}`
+            );
+    
+            const chord = player.findChord(position);
+            songInfoList.push(`コード： ${chord ? chord.name : "----"}`);
+    
+            const word = video.findWord(position);
+            songInfoList.push(
+                `声量(time): ${this.getFormatted(
+                    player.getVocalAmplitude(position)
+                )}`
+            );
+            let wordAmplitude = 0;
+            if (word) {
+                wordAmplitude =
+                    (player.getVocalAmplitude(word.startTime) +
+                        player.getVocalAmplitude(word.endTime) +
+                        player.getVocalAmplitude(
+                            (word.startTime + word.endTime) / 2
+                        )) /
+                    3;
+            }
+            songInfoList.push(`声量(word): ${this.getFormatted(wordAmplitude)}`);
+    
+            // Uncaught TypeError: Cannot read properties of undefined 回避のための処理
+            if (position < video.lastChar.endTime) {
+                const va = player.getValenceArousal(position);
+                songInfoList.push(
+                    `V/A(time): 感情価 ${this.getFormatted(
+                        va.v,
+                        4
+                    )}, 覚醒度 ${this.getFormatted(va.a, 4)}`
+                );
+            }    
+        } catch (error) {
+            // pass エラー回避のための対応
         }
 
         return songInfoList;
