27e0a7d5958371015f1007ca6524d4882191393d
sakuramodki <sakuramodki@gmail.com>
2021-09-23 16:45:07 +0900

    平滑化する時間幅を調整

--- textalive-sample/src/scenes/audioVisualizer/domain/service/visualizerService.ts
+++ textalive-sample/src/scenes/audioVisualizer/domain/service/visualizerService.ts
@@ -1,38 +1,41 @@
 import GainRepository from "../../infrastructure/repository/GainRepository";
 
+const TIME_SMOOTH = 20; // x 10msの時間で平滑化する
+
 export default class visualizerService {
-  private readonly size: number;
-  private readonly repository: GainRepository;
+    private readonly size: number;
+    private readonly repository: GainRepository;
 
-  constructor(size: number) {
-    this.size = size;
-    this.repository = new GainRepository(this.size);
-  }
+    constructor(size: number) {
+        this.size = size;
+        this.repository = new GainRepository(this.size);
+    }
 
-  public getGain(position: number): number[] {
-    // データソースからゲインを取得して [時間][周波数] の行列から [周波数][時間] の行列に変換する
-    const gains = this.transpose( this.repository.getGain(position ?? 0.0) );
+    public getGain(position: number): number[] {
+        // データソースからゲインを取得して [時間][周波数] の行列から [周波数][時間] の行列に変換する
+        const gains = this.transpose(this.repository.getGain(position ?? 0.0, TIME_SMOOTH));
 
-    // 周波数ごとにスムージングして出力
-    return gains.map(gain => this.smoothing(gain));
-  }
+        // 周波数ごとにスムージングして出力
+        return gains.map((gain) => this.smoothing(gain));
+    }
 
-  /**
-   * 入力された値を平滑化する
-   * @param value
-   * @private
-   */
-  private smoothing(value: number[]): number {
-    // 平均を取る
-    return value.reduce((acc, val) => acc + (val/value.length));
-  }
+    /**
+     * 入力された値を平滑化する
+     * @param value
+     * @private
+     */
+    private smoothing(value: number[]): number {
+        return Math.max(...value);
+        // 平均を取る
+        return value.reduce((acc, val) => acc + val / value.length, 0);
+    }
 
-  /**
-   * 配列の行列を入れ替える
-   * @param array
-   * @private
-   */
-  private transpose(array: number[][]): number[][] {
-    return array[0].map((_, column) => array.map(row => row[column]));
-  }
+    /**
+     * 配列の行列を入れ替える
+     * @param array
+     * @private
+     */
+    private transpose(array: number[][]): number[][] {
+        return array[0].map((_, column) => array.map((row) => row[column]));
+    }
 }
